37bd310d7e360587732c3c38d96cd50d92afda1d
rednav1979 <102362268+rednav1979@users.noreply.github.com>
2024-06-05 11:06:47 -0300

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,7 +1,7 @@
 <?php
    $host = "localhost";//caso esteja usando o xampp ou wamp
    $user = "";
-   $pass = "#";
+   $pass = "";
    $db = "";
    $conn = mysql_connect($host, $user, $pass) or die (mysql_error());
    
